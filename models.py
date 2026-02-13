e798cf4d08f2f251ca97590e11bdba596c8df798
4mh23cs014-cpu <4mh23cs014@gmail.com>
2026-02-13 15:46:04 +0530

    l;'l'

--- models.py
+++ models.py
@@ -1,3 +1,14 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String,Boolean
 
 Base = declarative_base()
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String,unique=True, index=True)
+    password = Column(String)
+    address = Column(String) 
+    phone = Column(String)
+    isprime= Column(Boolean,default=False)
+    
